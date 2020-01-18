21bddb4d21afe427c2ae22569febc900087277d3
Aliaksandr Shautsou <aliaksandr_shautsou@epam.com>
2020-01-18 18:34:18 +0300

    added cart item

--- screens/shop/CartScreen.js
+++ screens/shop/CartScreen.js
@@ -2,6 +2,7 @@ import React from "react";
 import { StyleSheet, View, Text, FlatList, Button } from "react-native";
 import { useSelector } from "react-redux";
 import colors from "../../constants/colors";
+import CartItem from "../../components/shop/CartItem";
 
 const CartScreen = props => {
   const cartTotalAmount = useSelector(state => state.cart.totalAmount);
@@ -30,9 +31,18 @@ const CartScreen = props => {
           disabled={cartItems.length <= 0}
         />
       </View>
-      <View>
-        <Text>CART ITEMS</Text>
-      </View>
+      <FlatList
+        data={cartItems}
+        keyExtractor={item => item.productId}
+        renderItem={itemData => (
+          <CartItem
+            quantity={itemData.item.quantity}
+            title={itemData.item.productTitle}
+            amount={itemData.item.sum}
+            onRemove={() => {}}
+          />
+        )}
+      />
     </View>
   );
 };
