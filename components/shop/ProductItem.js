8ca60eda9c4f1bab004b6570f5759ae7421ad578
Aliaksandr Shautsou <aliaksandr_shautsou@epam.com>
2020-01-19 21:15:44 +0300

    updating product item component

--- components/shop/ProductItem.js
+++ components/shop/ProductItem.js
@@ -4,13 +4,10 @@ import {
   Text,
   Image,
   StyleSheet,
-  Button,
   TouchableOpacity,
   TouchableNativeFeedback,
   Platform
 } from "react-native";
-
-import Colors from "../../constants/colors";
 
 const ProductItem = props => {
   let TouchableComponent = TouchableOpacity;
@@ -22,7 +19,7 @@ const ProductItem = props => {
   return (
     <View style={styles.product}>
       <View style={styles.touchable}>
-        <TouchableComponent onPress={props.onViewDetail} useForeground>
+        <TouchableComponent onPress={props.onSelect} useForeground>
           <View>
           <View style={styles.imageContainer}>
             <Image style={styles.image} source={{ uri: props.imageUrl }} />
@@ -32,16 +29,7 @@ const ProductItem = props => {
             <Text style={styles.price}>{props.price.toFixed(2)}$</Text>
           </View>
           <View style={styles.actions}>
-            <Button
-              color={Colors.primary}
-              title="View Details"
-              onPress={props.onViewDetail}
-            />
-            <Button
-              color={Colors.accent}
-              title="To Cart"
-              onPress={props.onAddToCart}
-            />
+            {props.children}
           </View>
           </View>
         </TouchableComponent>
